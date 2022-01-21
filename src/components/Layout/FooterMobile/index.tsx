4cb6441b783a57ab3d236ea442822f01a8066b62
st4l1nR <61132529+st4l1nR@users.noreply.github.com>
2022-01-20 21:50:02 -0500

    snackbar /contact-us added

--- src/components/Layout/FooterMobile/index.tsx
+++ src/components/Layout/FooterMobile/index.tsx
@@ -6,19 +6,8 @@ const index = () => {
     <div>
       {/*Footer Mobile*/}
       <div className="flex flex-col items-center py-5 mt-12 space-y-5 font-thin md:hidden text-white/70">
-        <img src="/icons/logo.svg" alt="" className="w-24" />
-        <Link href="/support">
-          <span className="cursor-pointer hover:text-primary">Support</span>
-        </Link>
-        <Link href="/policy">
-          <span className="cursor-pointer hover:text-primary">
-            Privacy Policy
-          </span>
-        </Link>
-        <Link href="/terms">
-          <span className="cursor-pointer hover:text-primary">
-            Terms and Conditions
-          </span>
+        <Link href="/">
+          <img src="/icons/logo.svg" alt="" className="w-24" />
         </Link>
       </div>
     </div>
